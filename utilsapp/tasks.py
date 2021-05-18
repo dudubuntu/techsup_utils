c7c75f72ec360d6af63c860a6c95e77e6c39e0a9
Денис <piligrim021@gmail.com>
2021-05-18 12:16:37 +0300

    small things + task clear_db

--- utilsapp/tasks.py
+++ utilsapp/tasks.py
@@ -8,16 +8,30 @@ from utilsapp.models import File
 
 
 @shared_task
+def clear_db(all=False):
+    if all:
+        File.objects.all().delete()
+        clear_files.delay()
+    files = File.objects.all().order_by('-created')
+    if not len(files) < 5:
+        for file in files[5:]:
+            file.delete()
+
+
+@shared_task
 def check_csv(psp_file_name, db_file_name, is_deposit):
     checkcsv = services.CheckCsv()
     checkcsv.check_neteller(psp_file_name, db_file_name, is_deposit=is_deposit)
     checkcsv.insert_file()
+    
+    clear_db.delay()
+
 
 @shared_task
 def clear_files(all=False):
     if all:
         pass
-    files_names = [file['name'] for file in File.objects.all().order_by('-created')[:3].values('file')]
+    files_names = [file['file'] for file in File.objects.all().order_by('-created')[:5].values('file')]
     dir_files = os.listdir(settings.MEDIA_ROOT)
     while dir_files:
         entry = dir_files.pop()
