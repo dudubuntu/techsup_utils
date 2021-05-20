c7edad0301736da37cf9845acc809292f4cf2dcf
Денис <piligrim021@gmail.com>
2021-05-20 15:13:58 +0300

    small changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,12 @@
 
 python manage.py makemigrations --no-input
 
+python manage.py makemigrations utilsapp --no-input
+
 python manage.py migrate --no-input
+
+python manage.py createsuperuser --no-input
+
+python manage.py collectstatic --no-input
 
 exec gunicorn utils.wsgi:application -b 0.0.0.0:8000 --reload
--- utils/settings.py
+++ utils/settings.py
@@ -138,10 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = PROJECT_ROOT / 'static'
-STATICFILES_DIRS = [
-    PROJECT_ROOT / 'static'
-]
+STATIC_ROOT = PROJECT_ROOT / 'static'
+# STATICFILES_DIRS = [
+#     PROJECT_ROOT / 'static'
+# ]
 
 
 MEDIA_URL = '/media/'
